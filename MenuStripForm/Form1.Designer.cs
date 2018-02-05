67f1179f1ab928b6861b885ab1b3558a5dab2e83
Jaydeep <jlakhawani@gmail.com>
2018-02-05 18:51:07 -0500

    Menustrips-form

--- MenuStripForm/Form1.Designer.cs
+++ MenuStripForm/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.file1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.file2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.file3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file1ToolStripMenuItem,
+            this.file2ToolStripMenuItem,
+            this.file3ToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // file1ToolStripMenuItem
+            // 
+            this.file1ToolStripMenuItem.Name = "file1ToolStripMenuItem";
+            this.file1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.file1ToolStripMenuItem.Text = "File1";
+            // 
+            // file2ToolStripMenuItem
+            // 
+            this.file2ToolStripMenuItem.Name = "file2ToolStripMenuItem";
+            this.file2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.file2ToolStripMenuItem.Text = "File2";
+            // 
+            // file3ToolStripMenuItem
+            // 
+            this.file3ToolStripMenuItem.Name = "file3ToolStripMenuItem";
+            this.file3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.file3ToolStripMenuItem.Text = "File3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem file1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem file2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem file3ToolStripMenuItem;
     }
 }
 
